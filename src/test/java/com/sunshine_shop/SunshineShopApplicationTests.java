08dcfccf6c4339e8de3ae7451bfb62196ce96993
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-10 00:00:27 +0700

    init project and inert layout of ant design library

---
+++ src/test/java/com/sunshine_shop/SunshineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunshine_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunshineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
